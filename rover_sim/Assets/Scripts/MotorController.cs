fa6d64c14271f636143526fc62ad7aebc5c512ef
Roozki <zawadzkirowan@gmail.com>
2024-10-10 04:05:21 -0700

    Got it working. Can now power motors from topics

--- rover_sim/Assets/Scripts/MotorController.cs
+++ rover_sim/Assets/Scripts/MotorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using ROS2;
 using System;
+// using GameObject;
 
 // Import ROS2 message types with aliases to avoid conflicts
 // using std_msgs = std_msgs.msg;
@@ -11,14 +12,18 @@ using System;
 public class MotorController : MonoBehaviour
 {
     // ROS2 Node and Subscribers
+    // private ROS2UnityComponent ros2Unity
     private ROS2Node rosNode;
     private ISubscription<std_msgs.msg.Float64MultiArray> motorSub;
     private ISubscription<geometry_msgs.msg.Twist> cmdVelSub;
 
     // Motor command array size
     private const int motorCount = 6;
+        ROS2UnityCore ros2Unity = new ROS2UnityCore();
 
     // Wheel joint names in the same order as motor commands
+
+    [SerializeField]
     private string[] wheelJointNames = new string[motorCount] {
         "back_left_wheel_link",
         "back_right_wheel_link",
@@ -27,8 +32,11 @@ public class MotorController : MonoBehaviour
         "right_front_wheel_link",
         "right_mid_wheel_link"
     };
-
+    // public List<string> childActuatorNames;    
+    // private List<ActuatorScript> actuators = new List<ActuatorScript>();
+// private Dictionary<string, ActuatorScript> actuators = new Dictionary<string, ActuatorScript>();
     // Wheel joints
+    [SerializeField]
     private ArticulationBody[] wheelJoints = new ArticulationBody[motorCount];
 
     // ROS2 Topic Names
@@ -37,19 +45,23 @@ public class MotorController : MonoBehaviour
     [SerializeField]
     private string cmdVelTopicName = "/cmd_vel";
 
+
+private Queue<std_msgs.msg.Float64MultiArray> motorCommandQueue = new Queue<std_msgs.msg.Float64MultiArray>();
+private readonly object queueLock = new object(); // Lock for thread safety
+
     void Start()
     {
+                        print("MeowStart");
+
         // Initialize ROS2
-        ROS2UnityCore ros2Unity = new ROS2UnityCore();
         if (ros2Unity.Ok())
         {
             rosNode = ros2Unity.CreateNode("unity_motor_controller");
-
             // Subscribe to motor commands topic
             motorSub = rosNode.CreateSubscription<std_msgs.msg.Float64MultiArray>(
                 motorTopicName,
                 msg => MotorCommandCallback(msg)
-            );
+                );
 
             // Subscribe to cmd_vel topic
             cmdVelSub = rosNode.CreateSubscription<geometry_msgs.msg.Twist>(
@@ -67,10 +79,16 @@ public class MotorController : MonoBehaviour
         {
             string jointName = wheelJointNames[i];
             // Use UnityEngine.Transform to avoid ambiguity
-            UnityEngine.Transform jointTransform = transform.Find(jointName);
-            if (jointTransform != null)
+            // UnityEngine.Transform jointTransform = transform.Find(jointName);
+            GameObject jointObject = GameObject.Find(jointName);
+
+            if (jointObject != null)
             {
-                wheelJoints[i] = jointTransform.GetComponent<ArticulationBody>();
+                wheelJoints[i] = jointObject.GetComponent<ArticulationBody>();
+                wheelJoints[i].gameObject.AddComponent<JointControl>();
+                wheelJoints[i].jointFriction = 1;
+                wheelJoints[i].angularDamping = 1;
+                // JointControl currentDrive = wheelJoints.xDrive;
                 if (wheelJoints[i] == null)
                 {
                     Debug.LogError($"ArticulationBody not found on joint {jointName}");
@@ -81,20 +99,32 @@ public class MotorController : MonoBehaviour
                 Debug.LogError($"Joint {jointName} not found in the scene");
             }
         }
+
     }
 
     // Update method may not be necessary if ROS2UnityCore handles spinning
     void Update()
+    {  
+        lock (queueLock)
     {
+        while (motorCommandQueue.Count > 0)
+        {
+            var msg = motorCommandQueue.Dequeue();
+            DriveMotors(msg);
+        }
+    }
         // If needed, you can call ros2Unity.SpinOnce() here
+        // ros2Unity.SpinOnce();
+        // std_msgs.msg.Float64MultiArray test_msg;
+        // std_msgs.msg.Float64MultiArray test_msg = new std_msgs.msg.Float64MultiArray();
+        // test_msg.Data = new double[6] {1, 1, 1, 1, 1, 1};
+        // MotorCommandCallback(test_msg);
+    // rosNode.SpinSome();
     }
 
-    // Callback for motor commands
-    private void MotorCommandCallback(std_msgs.msg.Float64MultiArray msg)
-    {
+     private void DriveMotors(std_msgs.msg.Float64MultiArray msg){
         double[] motorCommands = msg.Data;
-
-        if (motorCommands.Length != motorCount)
+       if (motorCommands.Length != motorCount)
         {
             Debug.LogWarning($"Expected {motorCount} motor commands, but received {motorCommands.Length}.");
             return;
@@ -110,16 +140,29 @@ public class MotorController : MonoBehaviour
                 var drive = joint.xDrive;
                 drive.targetVelocity = (float)command;
                 joint.xDrive = drive;
+                print("Meow");
             }
             else
             {
                 Debug.LogError($"Wheel joint at index {i} is null");
             }
         }
+     }
+
+    // Callback for motor commands
+    public void MotorCommandCallback(std_msgs.msg.Float64MultiArray msg)
+    {
+          
+lock (queueLock){
+  print("MeowCallbackLocked");
+          motorCommandQueue.Enqueue(msg);
+}
+
+ 
     }
 
     // Callback for cmd_vel messages
-    private void CmdVelCallback(geometry_msgs.msg.Twist msg)
+    public void CmdVelCallback(geometry_msgs.msg.Twist msg)
     {
         // Vector3 linear = new Vector3(
         //     (float)msg.Linear.X,
@@ -132,8 +175,15 @@ public class MotorController : MonoBehaviour
         //     (float)msg.Angular.Y,
         //     (float)msg.Angular.Z
         // );
-
-        // Debug.Log($"Received cmd_vel - Linear: {linear}, Angular: {angular}");
+        double linear = msg.Linear.X;
+        double angular = msg.Angular.Z;
+        double right = linear + angular;
+        double left = linear - angular;
+   // std_msgs.msg.Float64MultiArray test_msg;
+        std_msgs.msg.Float64MultiArray test_msg = new std_msgs.msg.Float64MultiArray();
+        test_msg.Data = new double[6] {left, left, left, right, right, right};
+        MotorCommandCallback(test_msg);
+        Debug.Log($"Received cmd_vel - Linear: {linear}, Angular: {angular}");
         // // TODO: Implement robot movement based on cmd_vel
         // // For example, convert linear and angular velocities to wheel velocities
         // and set the wheel joints' target velocities accordingly
--- rover_sim/Assets/Scripts/testnode.cs
+++ rover_sim/Assets/Scripts/testnode.cs
@@ -13,6 +13,7 @@ public class testnode : MonoBehaviour
 
     private ROS2Node hello_world_node;
     private IPublisher<std_msgs.msg.String> chatter_pub;
+    private ISubscription<std_msgs.msg.String> listen_sub;
     private int FRAMERATE = 30;
     private int framecount = 0;
  // Start is called before the first frame update
@@ -22,7 +23,16 @@ public class testnode : MonoBehaviour
         if (ros2Unity.Ok()) {
             hello_world_node = ros2Unity.CreateNode("ROS2UnityListenerNode");
                 chatter_pub = hello_world_node.CreatePublisher<std_msgs.msg.String>("chatter"); 
+                  listen_sub = hello_world_node.CreateSubscription<std_msgs.msg.String>(
+                "listener",
+                msg => stringCallback(msg)
+            );
         }
+    }
+
+    void stringCallback(std_msgs.msg.String msg){
+        // print(msg.data);
+        print("meowTEST");
     }
 
     // Update is called once per frame
@@ -34,7 +44,9 @@ public class testnode : MonoBehaviour
     msg.Data = "Hello Ros2ForUnity!";
     chatter_pub.Publish(msg);
     framecount = 0;
+    print("TESTNODECALLBACK");
         }
+
  
         
     }
