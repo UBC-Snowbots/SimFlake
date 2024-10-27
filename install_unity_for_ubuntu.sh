a2d503635d53d659df034e575fea5adb6cfebb9d
Roozki <zawadzkirowan@gmail.com>
2024-10-27 10:50:12 -0700

    just adding setup script

---
+++ install_unity_for_ubuntu.sh
@@ -0,0 +1,4 @@
+echo SORRY, THIS SCRIPT IS INCOMPLETE
+#wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+
+
