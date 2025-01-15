9776c22339cce4f89a2013ccbaecfd99febca58c
Connor Moynihan <connman1220@gmail.com>
2025-01-14 17:57:07 -0800

    Lidar working

--- rover_sim/Assets/Scripts/HorizontalLidarScanner.cs
+++ rover_sim/Assets/Scripts/HorizontalLidarScanner.cs
@@ -54,17 +54,40 @@ public class HorizontalLidarScanner : MonoBehaviour
         {
             for (int i = numberOfRays; i > 0; i--)
             {
-                float angle = i * (360f / numberOfRays);
+                float angle = i * ((360f) / numberOfRays);
                 Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, direction, out hit, scanRadius))
-                {
+                // RaycastHit hit;
+                if (Physics.Raycast(transform.position, direction, out var hit, scanRadius))
+                    {
                     Debug.Log($"Hit detected at {hit.point}");
                     ranges.Add(hit.distance);
+                    Debug.Log(hit.distance);
+
                     GameObject hitMarker = Instantiate(hitMarkerPrefab, hit.point, Quaternion.identity); // Instantiate the square
                     Destroy(hitMarker, hitMarkerLifetime); // Destroy the hit marker after a certain duration
+                } else {
+                ranges.Add(float.MaxValue);
                 }
+
             }
+
+
+            // var yawSensorDegrees = Mathf.Lerp(m_CurrentScanAngleStart, m_CurrentScanAngleEnd, t);
+            // var yawDegrees = yawBaseDegrees + yawSensorDegrees;
+            // var directionVector = Quaternion.Euler(0f, yawDegrees, 0f) * Vector3.forward;
+            // var measurementStart = RangeMetersMin * directionVector + transform.position;
+            // var measurementRay = new Ray(measurementStart, directionVector);
+            // var foundValidMeasurement = Physics.Raycast(measurementRay, out var hit, RangeMetersMax);
+            // // Only record measurement if it's within the sensor's operating range
+            // if (foundValidMeasurement)
+            // {
+            //     ranges.Add(hit.distance);
+            // }
+            // else
+            // {
+            //     ranges.Add(float.MaxValue);
+            // }
+            // }
 
            PublishImage();
 
@@ -84,10 +107,10 @@ public class HorizontalLidarScanner : MonoBehaviour
 
             Range_min = min,
             Range_max = max,
-            Angle_min = 0,
-            Angle_max = 360,
-            Angle_increment = (360 - 0) / numberOfRays,
-            Time_increment = 1,
+            Angle_min = 0 * Mathf.Deg2Rad,
+            Angle_max = 360 * Mathf.Deg2Rad,
+            Angle_increment = (2 * Mathf.PI)  / numberOfRays,
+            Time_increment = 1.0f / numberOfRays,
             Scan_time = (float)1,
             Intensities = new float[ranges.Count],
             Ranges = ranges.ToArray(),
